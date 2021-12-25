7bd49a1ce4f45bcec9a549e5acea51cd40467f55
chandyego <chandlerjuego101@gmail.com>
2021-12-25 21:52:59 +1000

    made preprogramming notes

--- Simpletron/Header.h
+++ Simpletron/Header.h
@@ -1,10 +1,28 @@
 #pragma once
 
-#include <iostream>
+#include <iostream> // cout, endl
+#include <iomanip> // setfill, setw
 #include <fstream>
 
 using std::cin;
 using std::cout;
 using std::endl;
+
+/*
+sets the showpos format flag for the str stream
+when showpos format flag is set, (+) sign precedes every non-negative 
+numerical value inserted into the stream
+*/
+using std::showpos; // shows (+) signs
+using std::noshowpos; // disable display of (+) signs
+/*
+for numbers w/ < 4 digits:
+* format numbers w/ leading zeros between the sign and the value
+* by using following statement before outputting the value:
+	cout << setfill('0') << internal;
+* stream manipulator "internal" indicates that fill characters should appear
+* between the sign and the numeric value
+* field width of fivve characters but does not have four digits (one position reserved for sign)
+*/
 
 using std::fstream;
--- Simpletron/SML.h
+++ Simpletron/SML.h
@@ -1,3 +1,46 @@
 #pragma once
 
 #include "Header.h"
+
+/*
+Variables:
+accumulator - accumulator register
+instructionCounter - tracks location in memory that contains instruction being performed
+operationCode - operation currently being performed ((left two digits of instruction)
+operand - memory location on which current instruction operates (rightmost two digits of instruction)
+instructionRegister - next instruction to be performed from memory
+
+-Do not execute instructions directly from memory
+-Transfer the next instruction to be performed from memory to variable called instructionRegister
+-Then, "pick off" the left two digits and place them in operationCode, 
+-"pick off" the right two digits and place them in operand
+
+Halt instruction:
+-Displays termination message
+-Causes SIMP to display the name and contents of each register
+-Also displays the complete contents of memory
+-^Printout is referred to as "register and memory dump"^
+
+Example of register and memory dump:
+
+REGISTERS:
+accumulator +0000
+instructionCounter 00
+instructionRegister +0000
+operationCode 00
+operand 00
+
+MEMORY:
+	   0	 1	   2     3     4     5     6     7     8     9
+ 0 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+10 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+20 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+30 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+40 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+50 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+60 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+70 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+80 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+90 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000 +0000
+*/
+
